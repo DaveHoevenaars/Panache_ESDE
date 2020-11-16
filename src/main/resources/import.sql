8477253a7e04a55daa1583d50361e7b514bb4ebc
Tobias Jansen <tobiasjansen2411@gmail.com>
2020-11-16 15:59:09 +0100

    added more test persons

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,16 +1,14 @@
---CREATE DATABASE test2;
---CREATE TABLE IF not exists person(
---     id int,
---     firstname varchar(36),
---     lastname varchar(36)
---);
-
 INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'George', 'Bluth');
 INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Michael', 'Bluth');
 INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Gob', 'Bluth');
 INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Buster', 'Bluth');
+INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Funke', 'Tobias');
+INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Funke', 'Lindsay ');
 
 INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'George', 'Bluth');
 INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Michael', 'Bluth');
 INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Gob', 'Bluth');
 INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Buster', 'Bluth');
+INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Funke', 'Tobias');
+INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Funke', 'lindsay ');
+
