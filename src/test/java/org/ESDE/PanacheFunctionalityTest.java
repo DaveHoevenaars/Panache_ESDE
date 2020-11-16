3a365dd9668ae02e2e0eba91d279ed51053d5cb4
DaveH001 <dave.hoevenaars@student.fontys.nl>
2020-11-16 15:22:39 +0100

    Implemented AR Testing

--- src/test/java/org/ESDE/PanacheFunctionalityTest.java
+++ src/test/java/org/ESDE/PanacheFunctionalityTest.java
@@ -1,14 +1,29 @@
 package org.ESDE;
 
+import io.quarkus.panache.mock.PanacheMock;
 import io.quarkus.test.junit.QuarkusTest;
+import org.ESDE.active_record.ARPerson;
+import org.ESDE.repository.RPerson;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 
 @QuarkusTest
 public class PanacheFunctionalityTest {
 
     @Test
-    public void testPanacheMocking(){
-
+    public void testPanacheMocking() {
+        // Mocked classes always return a default value
+        PanacheMock.mock(ARPerson.class);
+        Mockito.when(ARPerson.count()).thenCallRealMethod();
+        Assertions.assertEquals(4, ARPerson.count());
+    }
+    @Test
+    public void testCounting() {
+        // Now let's specify the return value
+        PanacheMock.mock(RPerson.class);
+        Mockito.when(ARPerson.count()).thenReturn(23l);
+        Assertions.assertEquals(23, ARPerson.count());
     }
 }
