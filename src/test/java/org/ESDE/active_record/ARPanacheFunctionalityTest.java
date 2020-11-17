3ab6902c7ec8256430f74ca91b6116b6a8c88a11
DaveH001 <dave.hoevenaars@student.fontys.nl>
2020-11-17 14:21:16 +0100

    Added ARPanacheFunctionalityTest and RPanacheFunctionalityTest

---
+++ src/test/java/org/ESDE/active_record/ARPanacheFunctionalityTest.java
@@ -0,0 +1,30 @@
+package org.ESDE.active_record;
+
+import io.quarkus.panache.mock.PanacheMock;
+import io.quarkus.test.junit.QuarkusTest;
+import org.ESDE.active_record.ARPerson;
+import org.ESDE.repository.RPerson;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+
+@QuarkusTest
+public class ARPanacheFunctionalityTest {
+
+    @Test
+    public void testPanacheMocking() {
+        // Mocked classes always return a default value
+        PanacheMock.mock(ARPerson.class);
+        Mockito.when(ARPerson.count()).thenCallRealMethod();
+        Assertions.assertEquals(4, ARPerson.count());
+    }
+    @Test
+    public void testCounting() {
+        // Now let's specify the return value
+        PanacheMock.mock(ARPerson.class);
+        Mockito.when(ARPerson.count()).thenReturn(23l);
+        Assertions.assertEquals(23, ARPerson.count());
+    }
+    //ToDo: test for filtering localhost:8080/ar/persons?key=lastname&val=Funke
+}
---
+++ src/test/java/org/ESDE/repository/RPanacheFunctionalityTest.java
@@ -0,0 +1,29 @@
+package org.ESDE.repository;
+
+import io.quarkus.panache.mock.PanacheMock;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import org.ESDE.repository.RPerson;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+
+@QuarkusTest
+public class RPanacheFunctionalityTest {
+    @InjectMock
+    RPersonRepository personRepository;
+
+    @Test
+    public void testPanacheMocking() {
+        // Mocked classes always return a default value
+        Assertions.assertEquals(4, personRepository.count());
+    }
+    @Test
+    public void testCounting() {
+        // Now let's specify the return value
+        Mockito.when(personRepository.count()).thenReturn(23l);
+        Assertions.assertEquals(23, personRepository.count());
+    }
+    //ToDo: test for filtering localhost:8080/ar/persons?key=lastname&val=Funke
+}
