75997d24883a6bcbf585cb0b0cea079286fb15a7
Tobias Jansen <tobiasjansen2411@gmail.com>
2020-11-17 15:22:07 +0100

    fixed fname lname mixup

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,13 +2,13 @@ INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequen
 INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Michael', 'Bluth');
 INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Gob', 'Bluth');
 INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Buster', 'Bluth');
-INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Funke', 'Tobias');
-INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Funke', 'Lindsay ');
+INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Tobias', 'Funke');
+INSERT INTO arperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Lindsay', 'Funke');
 
 INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'George', 'Bluth');
 INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Michael', 'Bluth');
 INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Gob', 'Bluth');
 INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Buster', 'Bluth');
-INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Funke', 'Tobias');
-INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Funke', 'lindsay ');
+INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'Tobias', 'Funke');
+INSERT INTO rperson (id, firstname, lastname) VALUES (nextVal('hibernate_sequence'), 'lindsay', 'Funke');
 
